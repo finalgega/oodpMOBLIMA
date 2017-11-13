7bd328e4d1fbae2ca37e3a5717ce4c6c71d43ecb
Kundo100 <karri-96@hotmail.com>
2017-11-13 22:29:55 +0800

    Add files via upload

--- Cinema.java
+++ Cinema.java
@@ -1,12 +1,26 @@
-package booking;
+package test;
 
 public class Cinema {
 	
+	 /**
+	 * The number of seats.
+	 */ 
+	
 	private int numberOfSeats;
+	
+	 /**
+	 * Creates a new Cinema with the given size.
+	 * @param num the number of seats in the cinema.
+	 */ 
 
 	public Cinema(int num){
 		numberOfSeats = num;
 	}
+	
+	/**
+	* Returns the number of seats in a cinema
+	@returns int - the number of seats in a cinema
+	**/
 	
 	public int getNumberOfSeats(){
 		return numberOfSeats;
--- MovieDisplay.java
+++ MovieDisplay.java
@@ -1,7 +1,18 @@
-package booking;
+package test;
 
 public class MovieDisplay {
+	
+	 /**
+	 * The layout of the cinema.
+	 */ 
+	
 	private Seat[][] seat;
+	
+	 /**
+	 * Initializes a new movie display and the layout for the display. 
+	 * Maximum number of seats is 210 (21 x 10).
+	 * @param num - the number of seats for the layout.
+	 */ 
 	
 	public MovieDisplay(int num){
 		seat = new Seat[num / 2][num]; 
@@ -13,6 +24,13 @@ public class MovieDisplay {
 			}			
 		}
 	}
+	
+	 /**
+	 * Assigns a seat to a customer. 
+	 * @param row - the row number a seat.
+	 * @param c - the letter of an aisle.
+	 * @param cust-id - the customer id.
+	 */ 
 	
 	public void assignSeat(int row, char c, int cust_id){
 		
@@ -48,6 +66,10 @@ public class MovieDisplay {
 		
 	}
 	
+	 /**
+	 * Displays the seats. 
+	 */ 
+	
 	public void displaySeats(){
 		
 		System.out.print("  ");
@@ -77,6 +99,11 @@ public class MovieDisplay {
 		}
 	}
 	
+	 /**
+	 * Unassigns a seat. 
+	 * @param cust-id - the seat id.
+	 */ 
+	
 	public void unAssignSeat(int seatId){
 		
 		Seat targetSeat = seat[0][0];
@@ -93,76 +120,6 @@ public class MovieDisplay {
 		
 		targetSeat.unAssign();
 		System.out.println("Seat Unassigned!");
-	}
-	
-	
-	/*
-	
-	public void showEmptySeats(){
-		System.out.println("The following seats are empty:");
-		
-		for(int i = 0; i < seat.length; i++){
-			if(!seat[i].isOccupied()){
-				System.out.println("SeatID " + seat[i].getSeatID());
-			}
-		}
-	}
-	
-	public Seat[] sortSeats(){
-		Seat[] a = seat.clone();
-		PlaneSeat temp;
-		
-		for(int i = 1; i < a.length; i++){
-			for(int j = i; j > 0; j--){
-				if(a[j].getCustomerId() < a[j-1].getCustomerId()){
-					temp = a[j];
-					a[j] = a[j - 1];
-					a[j - 1] = temp;
-				}else break;
-			}
-		}
-		return a;
-	}
-	
-	public void showNumEmptySeats(){
-		int count = 0;
-		for(int i = 0; i < seat.length; i++){
-			if(!seat[i].isOccupied()){
-				count++;
-			}
-		}
-		numEmptySeat = count;
-		System.out.println("There are " + numEmptySeat + " empty seats");
-	}
-	
-	public void showEmptySeats(){
-		System.out.println("The following seats are empty:");
-		
-		for(int i = 0; i < seat.length; i++){
-			if(!seat[i].isOccupied()){
-				System.out.println("SeatID " + seat[i].getSeatID());
-			}
-		}
-	}
-	
-	public void showAssignedSeats(boolean bySeatId){
-		System.out.println("The seat assignments are as follow:");
-		if(bySeatId){
-			for(int i = 0; i < seat.length; i++){
-				if(seat[i].isOccupied()){
-					System.out.println("  SeatID " + seat[i].getSeatID() + " assigned to CustomerID " + seat[i].getCustomerId());
-				};
-			}
-		}else{
-			PlaneSeat[] a = this.sortSeats();
-			for(int i = 0; i < seat.length; i++){
-				if(a[i].isOccupied()){
-					System.out.println("  SeatID " + a[i].getSeatID() + "assigned to CustomerID " + a[i].getCustomerId());
-				};
-			}
-		}
-	}
-	*/
-	
+	}	
 
 }
