2e624694c6949070e5bfd324c1c2c2253a90f250
heutzi <jonas.noblet@gmail.com>
2017-11-16 11:00:35 +0800

    Update FileIOController.java

--- src/cz2002/moblima/utilities/FileIOController.java
+++ src/cz2002/moblima/utilities/FileIOController.java
@@ -71,7 +71,7 @@ public class FileIOController {
         User u;
         for (int i = 0; i < uL.size(); i++) {
             u = uL.get(i);
-            lines.add(String.valueOf(u.getUserID()) + " " + u.getFirstName() + " " + u.getLastName());
+            lines.add(String.valueOf(u.getUserID()) + " " + u.getFirstName() + " " + u.getLastName()+ " " + uL.getPassword());
         }
 
         try {
